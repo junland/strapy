549e39753f3d6b2295ca931aaab203515bdc98d8
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-08-14 16:32:05 +0100

    targets: Rename ia to x86_64
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- targets/x86_64.sh
+++ targets/x86_64.sh
@@ -2,6 +2,8 @@
 
 . $(dirname $(realpath -s $0))/../lib/base.sh
 
+# This profile is intended for modern Intel CPUs, and should happily work on
+# AMD Zen+
 
 export SERPENT_TARGET_CFLAGS="-march=haswell -mtune=skylake -O3 -fPIC -mprefer-vector-width=128"
 export SERPENT_TARGET_CXXFLAGS="${SERPENT_TARGET_CFLAGS}"
