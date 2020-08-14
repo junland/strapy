080cbf8289a50a0c2b8d72bad45f3b2ba90dbe5a
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-08-14 16:28:25 +0100

    targets/rockchip: Support CRC, better documentation
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- targets/rockchip.sh
+++ targets/rockchip.sh
@@ -4,8 +4,8 @@
 
 
 # The rockchip build profile is intended for Rockchip RK3399, found in Pinebook Pro
-# export SERPENT_TARGET_CFLAGS="-march=armv8-a -mtune=cortex-a72 -O3 -fPIC"
-export SERPENT_TARGET_CFLAGS="-march=armv8-a -mtune=cortex-a72 -O3"
+# This happily supports Rasberry Pi 4 too.
+export SERPENT_TARGET_CFLAGS="-march=armv8-a+crc -mtune=cortex-a72 -O3"
 export SERPENT_TARGET_CXXFLAGS="${SERPENT_TARGET_CFLAGS}"
 export SERPENT_TARGET_LDFLAGS=""
 export SERPENT_TRIPLET="aarch64-serpent-linux-musl"
