352b884ae6bfa30ee17046a0873643b03c5e7862
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-08-15 16:48:25 +0100

    stage2/toolchain: Ensure linking against our own libffi
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- stage2/toolchain.sh
+++ stage2/toolchain.sh
@@ -30,13 +30,12 @@ pushd llvm
 mkdir build && pushd build
 unset CFLAGS CXXFLAGS
 
+export SERPENT_STAGE1_TREE=`getInstallDir "1"`
+
 # Our old libc++ is built likely against glibc, so we need to tell it again
 # that any new use of libc++ is via musl.
-export CFLAGS="${SERPENT_TARGET_CFLAGS} -D_LIBCPP_HAS_MUSL_LIBC -Wno-error"
-export CXXFLAGS="${SERPENT_TARGET_CXXFLAGS} -D_LIBCPP_HAS_MUSL_LIBC -Wno-error"
-
-
-export SERPENT_STAGE1_TREE=`getInstallDir "1"`
+export CFLAGS="${SERPENT_TARGET_CFLAGS} -L${SERPENT_STAGE1_TREE}/usr/lib -Wno-unused-command-line-argument -D_LIBCPP_HAS_MUSL_LIBC -Wno-error"
+export CXXFLAGS="${SERPENT_TARGET_CXXFLAGS} -L${SERPENT_STAGE1_TREE}/usr/lib -Wno-unused-command-line-argument -D_LIBCPP_HAS_MUSL_LIBC -Wno-error"
 
 
 [ -e "${SERPENT_CLANG_TABLEGEN}" ] || serpentFail "Cannot find stage1 tblgen"
