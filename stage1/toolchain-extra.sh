36217512d2240d1e51174f8a33800fd241721f0c
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-08-15 16:04:18 +0100

    stage1/toolchain-extra: Joint-effort to get libcxx/libcxxabi cross-compiled
    Original patchwork by @sunnylunk on issue #17, modifying the build to
    include libcxx/libcxxabi as well as libunwind.
    
    I then took a large hammer and modified cflags, cmake flags and patched
    the build to stop it looking at glibc symbols to complete our bootstrap
    of toolchain-extras for aarch64.
    
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- stage1/toolchain-extra.sh
+++ stage1/toolchain-extra.sh
@@ -9,7 +9,7 @@ set -e
 # Currently this is just libunwind and should be sufficient to get onto stage2.
 
 if [[ "${SERPENT_TARGET_ARCH}" == "${SERPENT_HOST}" ]]; then
-    printInfo "Skipping toolchain-extras on native architecture"
+    printInfo "Skipping toolchain-extra on native architecture"
     exit 0
 fi
 
@@ -19,14 +19,32 @@ export PATH="${SERPENT_INSTALL_DIR}/usr/bin:$PATH"
 export CC="clang"
 export CXX="clang++"
 
-export CFLAGS="${SERPENT_TARGET_CFLAGS}"
-export CXXFLAGS="${SERPENT_TARGET_CXXFLAGS}"
+export CFLAGS="${SERPENT_TARGET_CFLAGS} -L${SERPENT_INSTALL_DIR}/lib -L${SERPENT_INSTALL_DIR}/usr/lib -D_LIBCPP_HAS_MUSL_LIBC -Wno-error -Wno-macro-redefined -Wno-unused-command-line-argument"
+export CXXFLAGS="${SERPENT_TARGET_CXXFLAGS} -L${SERPENT_INSTALL_DIR}/lib -L${SERPENT_INSTALL_DIR}/usr/lib -D_LIBCPP_HAS_MUSL_LIBC -Wno-error -Wno-macro-redefined -Wno-unused-command-line-argument"
 export AR="llvm-ar"
 export RANLIB="llvm-ranlib"
 export STRIP="llvm-strip"
 
+export TOOLCHAIN_VERSION="10.0.1"
+ 
+extractSource libcxx
+extractSource libcxxabi
 extractSource libunwind
-pushd libunwind*
+extractSource llvm
+
+ln -sv "libcxx-${TOOLCHAIN_VERSION}.src" libcxx
+ln -sv "libcxxabi-${TOOLCHAIN_VERSION}.src" libcxxabi
+ln -sv "libunwind-${TOOLCHAIN_VERSION}.src" libunwind
+ln -sv "llvm-${TOOLCHAIN_VERSION}.src" llvm
+
+# Stop using glibc functionality through failed test
+# Prevents HAVE___CXA_THREAD_ATEXIT_IMPL being defined and the ensuing linking errors
+pushd libcxxabi
+patch -p1 < "${SERPENT_PATCHES_DIR}/libcxxabi_musl_exit.patch"
+popd
+
+pushd llvm
+
 
 mkdir build && pushd build
 cmake .. -G Ninja  \
@@ -36,16 +54,25 @@ cmake .. -G Ninja  \
     -DCMAKE_ASM_FLAGS="${CFLAGS}" \
     -DCMAKE_TRY_COMPILE_TARGET_TYPE=STATIC_LIBRARY \
     -DLLVM_CONFIG_PATH="${SERPENT_INSTALL_DIR}/usr/bin/llvm-config" \
+    -DLLVM_DEFAULT_TARGET_TRIPLE="${SERPENT_TRIPLET}" \
+    -DLLVM_ENABLE_PROJECTS='libunwind;libcxxabi;libcxx' \
+    -DLLVM_TARGETS_TO_BUILD="${SERPENT_TARGET_LLVM_BACKEND}" \
     -DCMAKE_C_COMPILER=clang \
     -DCMAKE_CXX_COMPILER=clang++ \
     -DCMAKE_AR="${SERPENT_INSTALL_DIR}/usr/bin/ar" \
     -DCMAKE_NM="${SERPENT_INSTALL_DIR}/usr/bin/llvm-nm" \
     -DCMAKE_RANLIB="${SERPENT_INSTALL_DIR}/usr/bin/llvm-ranlib" \
+    -DLIBCXX_HAS_MUSL_LIBC=ON \
+    -DLIBCXX_TARGET_TRIPLE="${SERPENT_TRIPLET}" \
+    -DLIBCXX_USE_COMPILER_RT=ON \
+    -DLIBCXXABI_TARGET_TRIPLE="${SERPENT_TRIPLET}" \
+    -DLIBCXXABI_USE_COMPILER_RT=ON \
+    -DLIBCXXABI_USE_LLVM_UNWINDER=ON \
     -DLIBUNWIND_TARGET_TRIPLE="${SERPENT_TRIPLET}" \
-    -DLIBUNWIND_USE_COMPILER_RT=ON
+    -DLIBUNWIND_USE_COMPILER_RT=ON \
+    -DCMAKE_CROSSCOMPILING=ON
 
-ninja -j "${SERPENT_BUILD_JOBS}"
-
+ninja -j "${SERPENT_BUILD_JOBS}" cxx cxxabi unwind
 
 printInfo "Installing libunwind"
 install -D -d -m 00755 "${SERPENT_INSTALL_DIR}/lib/"
