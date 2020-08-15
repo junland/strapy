eff86044263b347e7ca65d68934cc893672605e9
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-08-15 16:51:40 +0100

    stage2/toolchain: Use /lib too
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- stage2/toolchain.sh
+++ stage2/toolchain.sh
@@ -34,8 +34,8 @@ export SERPENT_STAGE1_TREE=`getInstallDir "1"`
 
 # Our old libc++ is built likely against glibc, so we need to tell it again
 # that any new use of libc++ is via musl.
-export CFLAGS="${SERPENT_TARGET_CFLAGS} -L${SERPENT_STAGE1_TREE}/usr/lib -Wno-unused-command-line-argument -D_LIBCPP_HAS_MUSL_LIBC -Wno-error"
-export CXXFLAGS="${SERPENT_TARGET_CXXFLAGS} -L${SERPENT_STAGE1_TREE}/usr/lib -Wno-unused-command-line-argument -D_LIBCPP_HAS_MUSL_LIBC -Wno-error"
+export CFLAGS="${SERPENT_TARGET_CFLAGS} -L${SERPENT_STAGE1_TREE}/usr/lib -L${SERPENT_STAGE1_TREE}/lib -Wno-unused-command-line-argument -D_LIBCPP_HAS_MUSL_LIBC -Wno-error"
+export CXXFLAGS="${SERPENT_TARGET_CXXFLAGS} -L${SERPENT_STAGE1_TREE}/usr/lib -L${SERPENT_STAGE1_TREE}/lib -Wno-unused-command-line-argument -D_LIBCPP_HAS_MUSL_LIBC -Wno-error"
 
 
 [ -e "${SERPENT_CLANG_TABLEGEN}" ] || serpentFail "Cannot find stage1 tblgen"
