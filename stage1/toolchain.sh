7f0a6c73b25b6bc065891515ae00af3677ef1649
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-08-17 18:34:34 +0100

    toolchain: Fix patching
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- stage1/toolchain.sh
+++ stage1/toolchain.sh
@@ -14,10 +14,6 @@ extractSource libunwind
 extractSource lld
 extractSource llvm
 
-pushd clang
-patch -p1 < "${SERPENT_PATCHES_DIR}/clang/0001-ToolChains-Linux-Use-correct-musl-path-on-Serpent-OS.patch"
-popd
-
 ln -sv "clang-${TOOLCHAIN_VERSION}.src" clang
 ln -sv "compiler-rt-${TOOLCHAIN_VERSION}.src" compiler-rt
 ln -sv "libcxx-${TOOLCHAIN_VERSION}.src" libcxx
@@ -25,6 +21,10 @@ ln -sv "libcxxabi-${TOOLCHAIN_VERSION}.src" libcxxabi
 ln -sv "libunwind-${TOOLCHAIN_VERSION}.src" libunwind
 ln -sv "lld-${TOOLCHAIN_VERSION}.src" lld
 ln -sv "llvm-${TOOLCHAIN_VERSION}.src" llvm
+
+pushd clang
+patch -p1 < "${SERPENT_PATCHES_DIR}/clang/0001-ToolChains-Linux-Use-correct-musl-path-on-Serpent-OS.patch"
+popd
 
 pushd llvm
 
