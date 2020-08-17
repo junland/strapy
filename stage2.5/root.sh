11f6a5775bba9d1de770f4b28c2806f9f28381f7
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-08-17 22:05:47 +0100

    stage2.5/root: Duplicate stage2 for 2.5 work
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- stage2.5/root.sh
+++ stage2.5/root.sh
@@ -4,3 +4,15 @@ set -e
 . $(dirname $(realpath -s $0))/common.sh
 
 printInfo "Configuring root filesystem"
+
+export SERPENT_STAGE2_TREE=`getInstallDir "2"`
+
+[ -e "${SERPENT_STAGE2_TREE}/usr/bin/clang" ] || serpentFail "Cannot find stage2 tree"
+
+install -D -d -m 00755 "${SERPENT_INSTALL_DIR}" || serpentFail "Could not construct stage2.5 installdir"
+
+printInfo "Duplicating stage2 for 2.5"
+
+rsync -aHP "${SERPENT_STAGE2_TREE}/." "${SERPENT_INSTALL_DIR}/." --delete
+
+printInfo "Duplicatoin completed"
