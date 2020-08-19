499aafb152c4d189069df05df6cce31ef7c00ddf
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-08-19 19:58:33 +0100

    stage3/common: All exports for native builds
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- stage3/common.sh
+++ stage3/common.sh
@@ -7,3 +7,12 @@ export SERPENT_STAGE_NAME="stage3"
 prepareBuild
 
 export TERM="xterm"
+export CC="clang"
+export CXX="clang++"
+export LD="ld.lld"
+export AR="llvm-ar"
+export RANLIB="llvm-ranlib"
+export STRIP="llvm-strip"
+export CFLAGS="${SERPENT_TARGET_CFLAGS}"
+export CXXFLAGS="${SERPENT_TARGET_CXXFLAGS}"
+export LDFLAGS="${SERPENT_TARGET_LDFLAGS}"
