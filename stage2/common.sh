d423da356f441ef35c788a47b90117684387a448
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-08-19 17:47:15 +0100

    lib: Absorb stage1 compiler activation to bootstrap stage3
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- stage2/common.sh
+++ stage2/common.sh
@@ -5,43 +5,6 @@ export SERPENT_STAGE_NAME="stage2"
 . $(dirname $(realpath -s $0))/../lib/build.sh
 
 # Set up stage2 specific requirements
-
-export SERPENT_STAGE1_TREE=`getInstallDir "1"`
-
-if [[ ! -e "${SERPENT_STAGE1_TREE}/usr/bin/clang" ]]; then
-    printError "No stage1 compiler found"
-    exit 1
-fi
-
-export PATH="${SERPENT_STAGE1_TREE}/usr/bin:$PATH"
-
-# Check its the right clang/llvm.
-SERPENT_LLVM_TARGET=`llvm-config --host-target`
-if [[ $? -ne 0 ]]; then
-    printError "Could not run llvm-config"
-    exit 1
-fi
-
-if [[ "${SERPENT_LLVM_TARGET}" != "${SERPENT_TRIPLET}" ]]; then
-    printError "Incorrect LLVM target: ${SERPENT_LLVM_TARGET}"
-    exit 1
-fi
-
-printInfo "Using clang/llvm target: ${SERPENT_LLVM_TARGET}"
-
-unset SERPENT_LLVM_TARGET
-
-export CC="clang"
-export CXX="clang++"
-export AR="llvm-ar"
-export RANLIB="llvm-ranlib"
-export STRIP="llvm-strip"
-
-export CFLAGS="${SERPENT_TARGET_CFLAGS} -I${SERPENT_INSTALL_DIR}/usr/include -L${SERPENT_STAGE1_TREE}/lib -L${SERPENT_INSTALL_DIR}/usr/lib -Wno-unused-command-line-argument -D_LIBCPP_HAS_MUSL_LIBC -Wno-error"
-export CXXFLAGS="${SERPENT_TARGET_CXXFLAGS} -I${SERPENT_INSTALL_DIR}/usr/include -L${SERPENT_STAGE1_TREE}/lib -L${SERPENT_INSTALL_DIR}/usr/lib -Wno-unused-command-line-argument -D_LIBCPP_HAS_MUSL_LIBC -Wno-error"
-export LDFLAGS="${SERPENT_TARGET_LDFLAGS} -L${SERPENT_STAGE1_TREE}/lib -L${SERPENT_INSTALL_DIR}/usr/lib"
-export PKG_CONFIG_PATH="${SERPENT_INSTALL_DIR}/usr/lib/pkgconfig:${SERPENT_INSTALL_DIR}/usr/share/pkgconfig"
-
-unset SERPENT_STAGE1_TREE
+activateStage1Compiler
 
 prepareBuild
