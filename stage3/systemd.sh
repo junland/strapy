c5604ba0d47bdf8c24d6fedbe383b2623581936a
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-08-27 21:10:29 +0100

    stage3/systemd: Partially unblock building
    We have to disable anything using gshadow, but that still
    fails to build, as stuff still tries to use it when disabled..
    
    We have an upstreamed patch for includes, and set a define
    to ensure if_ether.h doesn't completely foobar the build.
    
    Other bits are undesirable yet or requiring more build
    dependencies.
    
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- stage3/systemd.sh
+++ stage3/systemd.sh
@@ -6,11 +6,20 @@ set -e
 extractSource systemd
 serpentChrootCd systemd-*
 
-printInfo "Enabling libwildebeest workarounds"
-export CFLAGS="${CFLAGS} `serpentChroot pkg-config --cflags --libs libwildebeest` -Wno-unused-command-line-argument"
+pushd systemd-*
+# Ensure sys/file.h is used for LOCK_EX
+patch -p1 < "${SERPENT_PATCHES_DIR}/systemd/0001-partition-makefs-Include-missing-sys-file.h-header.patch"
 
+printInfo "Enabling libwildebeest workarounds"
+
+# If we don't enable __UAPI_DEF_ETHDR=0 then the private if_ether header gets used and breaks the wordl.
+export CFLAGS="${CFLAGS} `serpentChroot pkg-config --cflags --libs libwildebeest` -Wno-unused-command-line-argument -D__UAPI_DEF_ETHHDR=0"
+
+# Many fails are due to missing gshadow, which is our current priority after finishing stubs
+# Some fail for networking reasons and can be re-enabled in future
+# NSS we currently disable as we're not *yet* using it.
 printInfo "Configuring systemd"
-serpentChroot meson --prefix=/usr --buildtype=plain build
+serpentChroot meson --prefix=/usr --buildtype=plain build -Dgshadow=false -Dtmpfiles=false -Dnetworkd=false -Dtests=false -Dfuzz-tests=false -Dslow-tests=false -Dinstall-tests=false -Dresolve=false -Dldconfig=false -Duserdb=false -Dnss-systemd=false -Dnss-resolve=false -Dnss-mymachines=false -Dnss-myhostname=false
 
 printInfo "Building systemd"
 serpentChroot ninja -j "${SERPENT_BUILD_JOBS}" -C build
