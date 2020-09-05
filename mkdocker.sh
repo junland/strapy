c0a6420a9983b2be34a6b96d6186b7a736404976
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-09-05 20:22:24 +0100

    mkdocker: Update for tag information + stripping
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- mkdocker.sh
+++ mkdocker.sh
@@ -16,9 +16,17 @@ rm -fv root/config*
 rmdir root/build
 rmdir root/serpent
 
+# Trim it.
+set +e
+find root/usr/bin -type f | xargs -I{} strip {}
+find root/usr/libexec -type f | xargs -I{} strip {}
+find root/usr/lib -type -f -name "*.so*" | xargs -I{} strip {}
+rm -v root/usr/lib/*.a
+set -e
+
 # TODO: Stateless profile! This is ugly as sin.
 
 echo "alias ls='ls -F --color=auto'" >> "root/etc/profile"
-docker rmi staging || :
+docker rmi serpentos/staging:latest || :
 
-docker build --tag staging .
+docker build --tag serpentos/staging:latest .
