e4f69d46c44c0a006300d13abad1ee2bace46ad5
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-09-07 18:17:54 +0100

    stage3/linux-pam: Convince to build
    Configure is woefully broken and cannot handle certain functions
    being built-in with libc, passing "none required". It also
    doesn't define macro values correctly, so we have to manually
    redefine them for the build to succeed.
    
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- stage3/linux-pam.sh
+++ stage3/linux-pam.sh
@@ -6,10 +6,16 @@ set -e
 extractSource linux-pam
 serpentChrootCd Linux-PAM-*
 
+# Nuke quota.
+cd Linux-PAM-*
 
 printInfo "Configuring linux-pam"
+unset CONFIG_SITE
 
-serpentChroot ./configure \
+# Configure is *woefully* broken so we manually set up the required userype limits
+export CFLAGS="${CFLAGS} $(serpentChroot pkg-config --cflags --libs libwildebeest-pwd) -Wno-unused-command-line-argument -DPAM_USERTYPE_OVERFLOW_UID=65534 -DPAM_USERTYPE_SYSUIDMIN=101 -DPAM_USERTYPE_UIDMIN=1000"
+
+serpentChroot ac_cv_func_quotactl=0 ac_cv_search_dlopen="-lc" ac_cv_search_opendir="-lc" ac_cv_search_crypt="-lc" ./configure \
     --build="${SERPENT_TRIPLET}" \
     --host="${SERPENT_TRIPLET}" \
     --prefix=/usr \
@@ -18,11 +24,13 @@ serpentChroot ./configure \
     --bindir=/usr/bin \
     --sbindir=/usr/sbin \
     --datadir=/usr/share \
-    --disable-nis
+    --disable-nis \
+    --with-kernel-overflow-uid=65534
 
 
+serpentChroot sed -e '/PAM_USERTYPE_/d' -i config.h
 printInfo "Building linux-pam"
-serpentChroot make -j "${SERPENT_BUILD_JOBS}"
+serpentChroot make -j 1 V=1 VERBOSE=1
 
 printInfo "Installing linux-pam"
-serpentChroot make -j "${SERPENT_BUILD_JOBS}" install
+serpentChroot make -j 1 V=1 VERBOSE=1
