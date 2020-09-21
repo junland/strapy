4a8f2e926f6de896a3d0d144f74845a0f6993a09
Peter O'Connor <sunnyflunk@serpentos.com>
2020-09-21 11:28:01 +1000

    stage2: Add gcc
    Signed-off-by: Peter O'Connor <sunnyflunk@serpentos.com>

---
+++ stage2/gcc.sh
@@ -0,0 +1,45 @@
+#!/bin/true
+set -e
+
+. $(dirname $(realpath -s $0))/common.sh
+
+if [[ "${SERPENT_LIBC}" == "musl" ]]; then
+    printInfo "Skipping gcc with musl libc"
+    exit 0
+fi
+
+extractSource gcc
+cd gcc-*
+
+export PATH="${SERPENT_INSTALL_DIR}/usr/bin:$PATH"
+export CC="gcc"
+export CXX="g++"
+
+export CFLAGS="${SERPENT_TARGET_CFLAGS}"
+export CXXFLAGS="${SERPENT_TARGET_CXXFLAGS}"
+
+printInfo "Configuring gcc"
+mkdir build && pushd build
+../configure --prefix=/usr \
+    --libdir=/usr/lib \
+    --target="${SERPENT_TRIPLET}" \
+    --host="${SERPENT_HOST}" \
+    --disable-bootstrap \
+    --disable-multilib \
+    --enable-shared \
+    --enable-threads=posix \
+    --enable-gnu-indirect-function \
+    --enable-__cxa_atexit \
+    --enable-ld=default \
+    --enable-clocale=gnu \
+    --with-gcc-major-version-only \
+    --enable-linker-build-id  \
+    --with-linker-hash-style=gnu \
+    --with-gnu-ld \
+    --enable-languages=c,c++,lto
+
+printInfo "Building gcc"
+make -j "${SERPENT_BUILD_JOBS}"
+
+printInfo "Installing gcc"
+make -j "${SERPENT_BUILD_JOBS}" install DESTDIR="${SERPENT_INSTALL_DIR}"
