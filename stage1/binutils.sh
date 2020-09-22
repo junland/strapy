d733260e397a13561fa17d22764f59a795b1d8c9
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-09-22 22:16:46 +0100

    stage1/binutils: Enough to make gcc build
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- stage1/binutils.sh
+++ stage1/binutils.sh
@@ -37,3 +37,4 @@ make -j "${SERPENT_BUILD_JOBS}" tooldir=/usr all-bfd all-binutils all-ld all-gas
 printInfo "Installing binutils"
 make -j "${SERPENT_BUILD_JOBS}" tooldir=/usr install-bfd install-ld install-gas DESTDIR="${SERPENT_INSTALL_DIR}"
 install -m 00755 binutils/ar "${SERPENT_INSTALL_DIR}/usr/bin/${SERPENT_TRIPLET}-ar"
+install -m 00755 binutils/ranlib "${SERPENT_INSTALL_DIR}/usr/bin/${SERPENT_TRIPLET}-ranlib"
