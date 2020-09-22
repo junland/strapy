73744b226b862d002babf83f12c50609ba0d1f63
Peter O'Connor <sunnyflunk@serpentos.com>
2020-09-22 12:49:23 +1000

    stage2/gcc: Add gcc requirements
    Signed-off-by: Peter O'Connor <sunnyflunk@serpentos.com>

--- stage2/gcc.sh
+++ stage2/gcc.sh
@@ -11,6 +11,17 @@ fi
 extractSource gcc
 cd gcc-*
 
+printInfo "Extracting gcc requirements"
+extractSource mpfr
+extractSource mpc
+extractSource gmp
+extractSource isl
+
+ln -sv "mpfr-4.1.0" mpfr
+ln -sv "mpc-1.2.0" mpc
+ln -sv "gmp-6.2.0" gmp
+ln -sv "isl-0.21" isl
+
 export CC="gcc"
 export CXX="g++"
 
