28ad92387d616ebacf0641659a0690ad9b0a2715
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-09-24 02:35:00 +0100

    Revert "stage2/gcc: Build and install target-libgcc"
    This reverts commit eed008b85ef2c12eb29dd83a94122baf4f3cbed6.

--- stage2/gcc.sh
+++ stage2/gcc.sh
@@ -69,12 +69,6 @@ make -j "${SERPENT_BUILD_JOBS}" all-gcc
 printInfo "Installing gcc"
 make -j "${SERPENT_BUILD_JOBS}" install-gcc DESTDIR="${SERPENT_INSTALL_DIR}"
 
-printInfo "Building target libgcc"
-make -j "${SERPENT_BUILD_JOBS}" all-target-libgcc
-
-printInfo "Installing target libgcc"
-make -j "${SERPENT_BUILD_JOBS}" install-target-libgcc DESTDIR="${SERPENT_INSTALL_DIR}"
-
 printInfo "Installing default compiler links"
 for i in "gcc" "g++" ; do
     ln -sv "${SERPENT_TRIPLET}-${i}" "${SERPENT_INSTALL_DIR}/usr/bin/${i}"
