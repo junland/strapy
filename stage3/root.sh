dc2102f588fa6eddc5dbd0beb731332b2b98fdf6
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-09-24 11:45:05 +0100

    stage3/root: Only link dash if we have musl configuration
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- stage3/root.sh
+++ stage3/root.sh
@@ -42,4 +42,9 @@ ln -svf /proc/self/fd/2 "${SERPENT_INSTALL_DIR}"/dev/stderr
 ln -svf /proc/kcore "${SERPENT_INSTALL_DIR}"/dev/core
 
 printInfo "Stashing /bin/sh compat link"
-ln -svf /serpent/usr/bin/dash "${SERPENT_INSTALL_DIR}/usr/bin/sh"
+
+if [[ "${SERPENT_LIBC}" == "musl" ]]; then
+    ln -svf /serpent/usr/bin/dash "${SERPENT_INSTALL_DIR}/usr/bin/sh"
+else
+    ln -sfv /serpent/usr/bin/bash "${SERPENT_INSTALL_DIR}/usr/bin/sh"
+fi
