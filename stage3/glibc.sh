2473673763554ea02cd996374044142b28161067
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-09-24 11:55:14 +0100

    stage3: Add glibc build to make world go round
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

---
+++ stage3/glibc.sh
@@ -0,0 +1,44 @@
+#!/bin/true
+set -e
+
+. $(dirname $(realpath -s $0))/common.sh
+
+extractSource glibc
+cd glibc-*
+
+# Build only US UTF-8 locale for now
+echo "SUPPORTED_LOCALES=\
+en_US.UTF-8/UTF-8
+" > localedata/SUPPORTED
+
+export CC="gcc"
+export CXX="g++"
+
+export CFLAGS="${SERPENT_TARGET_CFLAGS}"
+export CXXFLAGS="${SERPENT_TARGET_CXXFLAGS}"
+
+export SERPENT_STAGE1_TREE=$(getInstallDir "1")
+export PATH="${SERPENT_STAGE1_TREE}/usr/binutils/bin:${PATH}"
+
+printInfo "Configuring glibc"
+mkdir build && pushd build
+../configure --prefix=/usr \
+    --target="${SERPENT_TRIPLET}" \
+    --host="${SERPENT_HOST}" \
+    --libdir=/usr/lib \
+    --sysconfdir=/etc \
+    --disable-multilib \
+    ac_cv_slibdir=/usr/lib \
+    ac_cv_prog_LD=ld.bfd \
+    ac_cv_prog_AR=${SERPENT_TRIPLET}-ar \
+    ac_cv_prog_RANLIB=${SERPENT_TRIPLET}-ranlib \
+    ac_cv_prog_AS=${SERPENT_TRIPLET}-as \
+    ac_cv_prog_NM=${SERPENT_TRIPLET}-nm \
+    libc_cv_forced_unwind=yes
+
+printInfo "Building glibc"
+make -j "${SERPENT_BUILD_JOBS}"
+
+printInfo "Installing glibc"
+make -j "${SERPENT_BUILD_JOBS}" install DESTDIR="${SERPENT_INSTALL_DIR}"
+make -j "${SERPENT_BUILD_JOBS}" localedata/install-locales DESTDIR="${SERPENT_INSTALL_DIR}"
