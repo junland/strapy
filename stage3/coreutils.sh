e17070496f8098573b0eaa73f270b80482f6ee9a
Peter O'Connor <sunnyflunk@serpentos.com>
2021-02-03 07:17:23 +0300

    coreutils: Add hack from LFS to fix build with new autotools
    Signed-off-by: Peter O'Connor <sunnyflunk@serpentos.com>

--- stage3/coreutils.sh
+++ stage3/coreutils.sh
@@ -11,6 +11,8 @@ pushd coreutils-*
 printInfo "Configuring coreutils"
 # Fix aarch64 SYS_getdents regression
 patch -p1 < "${SERPENT_PATCHES_DIR}/coreutils/coreutils_8_31_ls.patch"
+# LFS: Fix issue with autoconf 2.70 update
+echo '# deleted' > m4/std-gnu11.m4
 
 export FORCE_UNSAFE_CONFIGURE=1
 serpentChroot ./configure \
