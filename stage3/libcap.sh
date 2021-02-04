760f0306dab47196a4a10fce4c7b23859098569f
Peter O'Connor <sunnyflunk@serpentos.com>
2021-02-04 06:45:02 +0300

    stage3/libcap: Build with clang
    Signed-off-by: Peter O'Connor <sunnyflunk@serpentos.com>

--- stage3/libcap.sh
+++ stage3/libcap.sh
@@ -11,7 +11,7 @@ cd libcap-*
 sed -i Makefile -e '/\-C tests/d'
 
 printInfo "Building libcap"
-serpentChroot make -j "${SERPENT_BUILD_JOBS}"
+serpentChroot make -j "${SERPENT_BUILD_JOBS}" BUILD_CC=clang CC=clang
 
 printInfo "Installing libcap"
 serpentChroot make -j "${SERPENT_BUILD_JOBS}" install RAISE_SETFCAP=no prefix=/usr lib=lib
