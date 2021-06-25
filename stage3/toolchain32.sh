7379359b8602b6d350f5151f55e56ca0e22a77e1
Peter O'Connor <sunnyflunk@serpentos.com>
2021-06-25 08:25:38 +0300

    stage3: Build the 32bit llvm parts with gcc first
    These are needed with the clang defaults to build 32bit anything with clang
    
    Signed-off-by: Peter O'Connor <sunnyflunk@serpentos.com>

---
+++ stage3/toolchain32.sh
@@ -0,0 +1,60 @@
+#!/bin/true
+set -e
+
+. $(dirname $(realpath -s $0))/common.sh
+
+export TOOLCHAIN_VERSION="12.0.0"
+
+printInfo "Extracting toolchain requirements"
+extractSource llvmorg
+
+mkdir -p  llvm-project-${TOOLCHAIN_VERSION}.src/llvm/build
+serpentChrootCd llvm-project-${TOOLCHAIN_VERSION}.src/llvm/build
+
+unset CFLAGS CXXFLAGS
+export CC="gcc"
+export CXX="g++"
+
+echo "cmake -G Ninja ../ \
+    -DCMAKE_INSTALL_PREFIX=/usr \
+    -DLLVM_ENABLE_PROJECTS='compiler-rt;libcxx;libcxxabi;libunwind' \
+    -DLLVM_LIBDIR_SUFFIX= \
+    -DLIBCXX_LIBDIR_SUFFIX=32 \
+    -DLIBCXXABI_LIBDIR_SUFFIX=32 \
+    -DLIBUNWIND_LIBDIR_SUFFIX=32 \
+    -DLLVM_BUILD_32_BITS=ON \
+    -DLIBCXX_INSTALL_SUPPORT_HEADERS=ON \
+    -DCOMPILER_RT_BUILD_LIBFUZZER=OFF \
+    -DCOMPILER_RT_BUILD_PROFILE=OFF \
+    -DCOMPILER_RT_BUILD_SANITIZERS=OFF \
+    -DCOMPILER_RT_BUILD_XRAY=OFF \
+    -DLLVM_USE_SANITIZER=OFF \
+    -DLLVM_ENABLE_UNWIND_TABLES=OFF \
+    -DLLVM_INCLUDE_BENCHMARKS=OFF \
+    -DLLVM_INCLUDE_TESTS=OFF \
+    -DLLVM_INCLUDE_UTILS=OFF \
+    -DLLVM_BUILD_TOOLS=OFF \
+    -DLIBCXX_ENABLE_SHARED=ON \
+    -DLIBCXX_ENABLE_STATIC=OFF \
+    -DLIBCXX_USE_COMPILER_RT=OFF \
+    -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=ON \
+    -DLIBCXX_STATICALLY_LINK_ABI_IN_STATIC_LIBRARY=ON \
+    -DLIBCXXABI_USE_LLVM_UNWINDER=OFF \
+    -DLIBCXXABI_ENABLE_STATIC_UNWINDER=ON \
+    -DLIBCXXABI_USE_COMPILER_RT=OFF \
+    -DLIBCXXABI_ENABLE_SHARED=OFF \
+    -DLIBCXXABI_INSTALL_LIBRARY=OFF \
+    -DLIBCXXABI_STATICALLY_LINK_UNWINDER_IN_STATIC_LIBRARY=ON \
+    -DLIBUNWIND_ENABLE_SHARED=ON \
+    -DLIBUNWIND_ENABLE_STATIC=ON \
+    -DLIBUNWIND_USE_COMPILER_RT=OFF \
+    -DLIBUNWIND_INSTALL_LIBRARY=ON" > llvm-project-${TOOLCHAIN_VERSION}.src/llvm/build/build.sh
+chmod +x llvm-project-${TOOLCHAIN_VERSION}.src/llvm/build/build.sh
+serpentChroot ./build.sh 
+
+
+printInfo "Building toolchain"
+serpentChroot ninja -j "${SERPENT_BUILD_JOBS}" -v compiler-rt cxx cxxabi unwind
+
+printInfo "Installing toolchain"
+serpentChroot ninja -j "${SERPENT_BUILD_JOBS}" -v install-compiler-rt install-cxx install-unwind
