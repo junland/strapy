d221992ef97ec074cb9ec4cd0a16aec395f57200
Peter O'Connor <sunnyflunk@serpentos.com>
2021-07-29 04:56:03 +0300

    Update toolchain to latest versions
    Signed-off-by: Peter O'Connor <sunnyflunk@serpentos.com>

--- stage2/toolchain.sh
+++ stage2/toolchain.sh
@@ -3,7 +3,7 @@ set -e
 
 . $(dirname $(realpath -s $0))/common.sh
 
-export TOOLCHAIN_VERSION="12.0.0"
+export TOOLCHAIN_VERSION="12.0.1"
 
 printInfo "Extracting toolchain requirements"
 extractSource llvmorg
