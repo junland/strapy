6dac42e713eb7b7d25df842e403edbddd938127e
Peter O'Connor <sunnyflunk@serpentos.com>
2021-10-04 20:56:26 +1100

    targets: Add v2 and v3 targets, v2 now the default
    v3 will become the optimization focus post build out of packages
    
    Signed-off-by: Peter O'Connor <sunnyflunk@serpentos.com>

--- targets/x86_64-v3.sh
+++ targets/x86_64-v3.sh
@@ -2,9 +2,10 @@
 
 . $(dirname $(realpath -s $0))/../lib/base.sh
 
-# This profile is intended for all CPUs made in last 10 years
+# This profile is intended for modern Intel CPUs, and should happily work on
+# AMD Zen+
 
-export SERPENT_TARGET_CFLAGS="-march=x86-64-v2 -mtune=ivybridge -O2 -fPIC -pipe"
+export SERPENT_TARGET_CFLAGS="-march=haswell -mtune=skylake -O2 -fPIC -pipe -mprefer-vector-width=128"
 export SERPENT_TARGET_CXXFLAGS="${SERPENT_TARGET_CFLAGS}"
 export SERPENT_TARGET_LDFLAGS=""
 export SERPENT_TRIPLET="x86_64-serpent-linux"
