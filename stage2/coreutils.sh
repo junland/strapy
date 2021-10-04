f558fd4d60c43ad9dd6fd4efd1b755f4c7d8df41
Peter O'Connor <sunnyflunk@serpentos.com>
2021-10-04 14:20:13 +0300

    coreutils: Remove workarounds with new release
    Signed-off-by: Peter O'Connor <sunnyflunk@serpentos.com>

--- stage2/coreutils.sh
+++ stage2/coreutils.sh
@@ -8,11 +8,6 @@ cd coreutils-*
 
 
 printInfo "Configuring coreutils"
-# Fix aarch64 SYS_getdents regression
-patch -p1 < "${SERPENT_PATCHES_DIR}/coreutils/coreutils_8_31_ls.patch"
-# LFS: Fix issue with autoconf 2.70 update
-echo '# deleted' > m4/std-gnu11.m4
-
 # Disable broke manpages
 sed -i Makefile.am -e '/man\/local.mk/d'
 autoreconf -vfi
--- stage3/coreutils.sh
+++ stage3/coreutils.sh
@@ -9,11 +9,6 @@ pushd coreutils-*
 
 
 printInfo "Configuring coreutils"
-# Fix aarch64 SYS_getdents regression
-patch -p1 < "${SERPENT_PATCHES_DIR}/coreutils/coreutils_8_31_ls.patch"
-# LFS: Fix issue with autoconf 2.70 update
-echo '# deleted' > m4/std-gnu11.m4
-
 export FORCE_UNSAFE_CONFIGURE=1
 serpentChroot ./configure \
     --build="${SERPENT_TRIPLET}" \
